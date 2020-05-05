2b85e0348c00cc4ff4b2965756fb8facae56d3a5
MaxCouling <mcouling@stu.kerikerihigh.ac.nz>
2020-05-05 10:18:48 +0000

    fixed totaling error

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import os
-i_item = i_money = i_time = e_item = e_money = e_time = []#defining all the lists as a list
+from tabulate import tabulate
+i_item = []
+i_money = []
+i_time =  []
+e_item = []
+e_time = []#defining all the lists as a list
+e_money=[]
 while True:#while this is running
   os.system("clear")#clears os
   incomeitem = input("income item press enter to exit\n")
-  if incomeitem == "":
-    
-    
+  if incomeitem == "":#if the user inputs an enter it breaks the while loop
     break
-  else:
-    
-    
-    i_item.append(incomeitem)
 
-   
-    i_money.append (int(input("money assoatied\n")))
+  else:
+    i_item.append(incomeitem)#adds the income item to the income item(i_item) list
+    i_money.append (int(input("money assoatied\n")))#asks for money for the item
     i_time.append(input("time assoated\n"))#1 2 or 3
     continue
+
 while True:
   os.system("clear")
   expenseitem = input("expense item press enter to exit\n")
@@ -31,16 +33,17 @@ while True:
     continue
 os.system("clear")
 
-
 incometable= []
 expensetable = []
 for k in range(len(i_item)):
   incometable.append([i_item[k],i_money[k],i_time[k]],)
-
 incometable.append(["TOTAL",sum(i_money)])
+
+#incometable.append("TOTAL",sum(i_money))
 for i in range(len(e_item)):
   expensetable.append([e_item[i],e_money[i],e_time[i]],)
 expensetable.append(["TOTAL",sum(e_money)])
+
 
 
 print(incometable.sort(reverse = True))
