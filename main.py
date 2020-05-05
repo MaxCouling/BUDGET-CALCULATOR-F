0d3e5f742f90f77bfdf8e0a3642b9db408d72f3c
MaxCouling <mcouling@stu.kerikerihigh.ac.nz>
2020-05-05 09:51:39 +0000

    table

--- main.py
+++ main.py
@@ -29,3 +29,25 @@ while True:
     e_money.append(int(input("money assoatied\n")))
     e_time.append(input("time assoated\n"))#1 2 or 3
     continue
+os.system("clear")
+
+
+incometable= []
+expensetable = []
+for k in range(len(i_item)):
+  incometable.append([i_item[k],i_money[k],i_time[k]],)
+
+incometable.append(["TOTAL",sum(i_money)])
+for i in range(len(e_item)):
+  expensetable.append([e_item[i],e_money[i],e_time[i]],)
+expensetable.append(["TOTAL",sum(e_money)])
+
+
+print(incometable.sort(reverse = True))
+
+print('\nINCOME\n')
+print(tabulate(incometable, headers=["Item","Money", "Time"]))
+print("\nEXPENSES\n")
+print(tabulate(expensetable, headers=["Item","Money", "Time"]))
+print("\nWHAT'S LEFT\n")
+print(tabulate([["total",sum(i_money)-sum(e_money)]],headers=()))
